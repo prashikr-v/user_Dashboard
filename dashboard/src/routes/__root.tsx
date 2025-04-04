94d766144bf0a6d2df80c4ec52279d5dd7f374e7
Prashik Rakhade <prashik.rakhade@vervelo.com>
2025-04-04 19:18:21 +0530

    fetched data from api

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -5,9 +5,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
+       
         
       </div>
       <hr />
