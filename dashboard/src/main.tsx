8d39c7d23279084712fd8c46a1d849d011af6f4e
Prashik Rakhade <prashik.rakhade@vervelo.com>
2025-04-04 14:57:48 +0530

    created  repo structure

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -4,6 +4,8 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MantineProvider } from '@mantine/core'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -15,13 +17,18 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient();
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
+      <QueryClientProvider client={queryClient}>
+      <MantineProvider >
       <RouterProvider router={router} />
+      </MantineProvider>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
